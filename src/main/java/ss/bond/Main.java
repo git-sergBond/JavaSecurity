fdf505afc82b6cac6495fe684448bf10ccb84a94
git-sergBond <git-serg.bond@yandex.ru>
2022-02-20 20:32:41 +0300

    Cipher init encrypt

--- src/main/java/ss/bond/Main.java
+++ src/main/java/ss/bond/Main.java
@@ -5,6 +5,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 
@@ -12,21 +13,31 @@ public class Main {
 
     public static void main(String[] args) {
         Security.addProvider(new BouncyCastleProvider());
+
+        Cipher cipher = null;
         try {
             String cipherAlgorithm = "AES";
             String cipherMode = "CBC";
             String cipherPadding = "PKCS5Padding";
             String cipherTransformation = String.join("/", cipherAlgorithm, cipherMode, cipherPadding);
-            Cipher cipher = Cipher.getInstance(cipherTransformation);
+            cipher = Cipher.getInstance(cipherTransformation);
             System.out.println("cipher(" + cipherTransformation +") success");
-
-            byte[] keyBytes = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-            String keyAlgorithm = "RawBytes";
-            SecretKeySpec key = new SecretKeySpec(keyBytes, keyAlgorithm);
-            System.out.println("key(" + keyAlgorithm +") success");
         } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
+
+        byte[] keyBytes = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        String keyAlgorithm = "RawBytes";
+        SecretKeySpec key = new SecretKeySpec(keyBytes, keyAlgorithm);
+        System.out.println("key(" + keyAlgorithm +") success");
+
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            System.out.println("cipher initialised");
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("test");
     }
 }
