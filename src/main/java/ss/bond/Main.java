fbc6d98ad0529c51ec124c54608d8750762b6e5f
git-sergBond <git-serg.bond@yandex.ru>
2022-02-20 20:28:43 +0300

    new Key RawBytes

--- src/main/java/ss/bond/Main.java
+++ src/main/java/ss/bond/Main.java
@@ -4,6 +4,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 
@@ -12,8 +13,17 @@ public class Main {
     public static void main(String[] args) {
         Security.addProvider(new BouncyCastleProvider());
         try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            System.out.println("cipher success");
+            String cipherAlgorithm = "AES";
+            String cipherMode = "CBC";
+            String cipherPadding = "PKCS5Padding";
+            String cipherTransformation = String.join("/", cipherAlgorithm, cipherMode, cipherPadding);
+            Cipher cipher = Cipher.getInstance(cipherTransformation);
+            System.out.println("cipher(" + cipherTransformation +") success");
+
+            byte[] keyBytes = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+            String keyAlgorithm = "RawBytes";
+            SecretKeySpec key = new SecretKeySpec(keyBytes, keyAlgorithm);
+            System.out.println("key(" + keyAlgorithm +") success");
         } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
