53809627c2a3ee7a8c80dc672a15640747e12fac
git-sergBond <git-serg.bond@yandex.ru>
2022-02-20 20:21:41 +0300

    new Cipher

--- src/main/java/ss/bond/Main.java
+++ src/main/java/ss/bond/Main.java
@@ -2,12 +2,21 @@ package ss.bond;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 
 public class Main {
 
     public static void main(String[] args) {
         Security.addProvider(new BouncyCastleProvider());
-        System.out.println("asdasd");
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            System.out.println("cipher success");
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        System.out.println("test");
     }
 }
