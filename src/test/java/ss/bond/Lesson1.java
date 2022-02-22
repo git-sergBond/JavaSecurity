b5adff39d87d116ed243c09ed66aa7b324e35e1b
git-sergBond <git-serg.bond@yandex.ru>
2022-02-22 13:06:08 +0300

    move to test

--- src/test/java/ss/bond/Lesson1.java
+++ src/test/java/ss/bond/Lesson1.java
@@ -1,6 +1,7 @@
 package ss.bond;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.Test;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -21,9 +22,47 @@ import java.security.SignatureException;
 import java.util.Arrays;
 import java.util.Objects;
 
-public class Main {
+public class Lesson1 {
 
-    public static void main(String[] args) {
+    @Test
+    public void encryptTest() {
+        Security.addProvider(new BouncyCastleProvider());
+
+        Cipher cipher = null;
+        try {
+            String cipherAlgorithm = "AES";
+            String cipherMode = "CBC";
+            String cipherPadding = "PKCS5Padding";
+            String cipherTransformation = String.join("/", cipherAlgorithm, cipherMode, cipherPadding);
+            cipher = Cipher.getInstance(cipherTransformation);
+            System.out.println("cipher(" + cipherTransformation +") success");
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        byte[] keyBytes = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        String keyAlgorithm = "RawBytes";
+        SecretKeySpec key = new SecretKeySpec(keyBytes, keyAlgorithm);
+        System.out.println("key(" + keyAlgorithm +") success");
+
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            System.out.println("cipher initialised");
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+
+        byte[] plainText = "it is my secret".getBytes(StandardCharsets.UTF_8);
+        try {
+            byte[] cipherText = cipher.doFinal(plainText);
+            System.out.println("cipherText=" + new String(cipherText, StandardCharsets.UTF_8));
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void signatureTest() {
         Security.addProvider(new BouncyCastleProvider());
 
         //KeyPair
@@ -71,7 +110,10 @@ public class Main {
             System.err.println("messageDigest is NULL");
             return;
         }
-        SecretKeySpec key = getSecretKeySpec();
+        byte[] keyBytes = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        String keyAlgorithm = "RawBytes";
+        SecretKeySpec key = new SecretKeySpec(keyBytes, keyAlgorithm);
+        System.out.println("key(" + keyAlgorithm +") success");
         try {
             mac.init(key);
         } catch (InvalidKeyException e) {
@@ -142,50 +184,5 @@ public class Main {
             e.printStackTrace();
         }
         System.out.println("verified: " + verified);
-
-
-
-        System.out.println("test");
-    }
-
-    private static void encryptExample() {
-        Security.addProvider(new BouncyCastleProvider());
-
-        Cipher cipher = null;
-        try {
-            String cipherAlgorithm = "AES";
-            String cipherMode = "CBC";
-            String cipherPadding = "PKCS5Padding";
-            String cipherTransformation = String.join("/", cipherAlgorithm, cipherMode, cipherPadding);
-            cipher = Cipher.getInstance(cipherTransformation);
-            System.out.println("cipher(" + cipherTransformation +") success");
-        } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        SecretKeySpec key = getSecretKeySpec();
-
-        try {
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            System.out.println("cipher initialised");
-        } catch (InvalidKeyException e) {
-            e.printStackTrace();
-        }
-
-        byte[] plainText = "it is my secret".getBytes(StandardCharsets.UTF_8);
-        try {
-            byte[] cipherText = cipher.doFinal(plainText);
-            System.out.println("cipherText=" + new String(cipherText, StandardCharsets.UTF_8));
-        } catch (IllegalBlockSizeException | BadPaddingException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static SecretKeySpec getSecretKeySpec() {
-        byte[] keyBytes = new byte[]{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        String keyAlgorithm = "RawBytes";
-        SecretKeySpec key = new SecretKeySpec(keyBytes, keyAlgorithm);
-        System.out.println("key(" + keyAlgorithm +") success");
-        return key;
     }
 }
