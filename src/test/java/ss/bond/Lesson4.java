3541ea20d2565df488931d78dfbaca2e8eb7cb19
git-sergBond <git-serg.bond@yandex.ru>
2022-02-26 20:21:46 +0300

    Mac

--- src/test/java/ss/bond/Lesson4.java
+++ src/test/java/ss/bond/Lesson4.java
@@ -2,10 +2,29 @@ package ss.bond;
 
 import org.junit.jupiter.api.Test;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 public class Lesson4 {
 
     @Test
-    public void mac() {
+    public void mac() throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac mac = Mac.getInstance("HmacSHA256");
 
+        byte[] keyBytes = new byte[] {0,1,2,3,4,5,6,7,8 ,9,10,11,12,13,14,15};
+        SecretKeySpec key = new SecretKeySpec(keyBytes, "RawBytes");
+
+        mac.init(key);
+
+        byte[] dataBytes1 = "qwertyasdfgh12345".getBytes(StandardCharsets.UTF_8);
+        byte[] dataBytes2 = "asdasdgfgfgfgfgfg".getBytes(StandardCharsets.UTF_8);
+
+        mac.update(dataBytes1);
+        byte[] macBytes = mac.doFinal(dataBytes2);
+
+        assert macBytes != null;
     }
 }
